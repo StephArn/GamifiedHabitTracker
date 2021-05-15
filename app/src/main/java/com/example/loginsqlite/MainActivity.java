14e8de1811070f407acea77b119217749dff492f
StephArn <stefana.arina@gmail.com>
2021-05-16 01:32:14 +0300

    Finished sign-up + log-in + DB

--- app/src/main/java/com/example/loginsqlite/MainActivity.java
+++ app/src/main/java/com/example/loginsqlite/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.loginsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username, password, repassword;
     Button signup, login;
+    DBHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +26,36 @@ public class MainActivity extends AppCompatActivity {
         repassword =  (EditText) findViewById(R.id.repassword);
         signup = (Button) findViewById(R.id.bttnsignup);
         login = (Button) findViewById(R.id.bttnlogin);
+        db = new DBHelper(this);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("") || pass.equals("") || repass.equals("") )
+                    Toast.makeText(MainActivity.this, "Please fill-in everything.", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = db.checkusername(user);
+                        if(!checkuser){
+                            Boolean insert = db.insertData(user,pass);
+                            if(insert){
+                                Toast.makeText(MainActivity.this, "Succes on registration!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(MainActivity.this,"Failed to register.", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this,"This user already exists.", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this,"Passwords do not match.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
@@ -34,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
